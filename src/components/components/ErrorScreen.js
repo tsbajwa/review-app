d8a18b2b077e98c3776a6605e8da363fb3e25988
tsbajwa <teranjit@gmail.com>
2017-11-14 20:32:50 +1100

    basic error screen styling

--- src/components/components/ErrorScreen.js
+++ src/components/components/ErrorScreen.js
@@ -3,9 +3,12 @@ import React from "react";
 export default function ErrorScreen(props) {
   //TODO: Need some distinction when button pressed and its Error again, maybe ErrorCount??
   return (
-    <div>
-      <div>Sorry something went wrong. please try again </div>
-      <button onClick={props.getReviews}>Click here to load again</button>
+    <div className="error-screen-container">
+      <h1>Sorry something went wrong. Click below to load reviews. </h1>
+
+      <button className="error-screen-container__btn" onClick={props.getReviews}>
+        Load Reviews
+      </button>
     </div>
   );
 }
