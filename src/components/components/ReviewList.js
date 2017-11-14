9a60c4e35ecc2c751b0574f23c006f5b34fb02fc
tsbajwa <teranjit@gmail.com>
2017-11-14 23:28:56 +1100

    minor cleanup

--- src/components/components/ReviewList.js
+++ src/components/components/ReviewList.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReviewSingleContainer from "../containers/ReviewSingleContainer";
 
 export default function reviewList({ sortedReviews }) {
-  const renderList = () => {
-    return sortedReviews.map((review, index) => {
-      return <ReviewSingleContainer review={review} key={index} />;
-    });
-  };
-
-  return <div className="review-list-container">{renderList()}</div>;
+  console.log(sortedReviews.length);
+  return (
+    <div className="review-list-container">
+      {sortedReviews.length ? (
+        sortedReviews.map((review, index) => <ReviewSingleContainer review={review} key={index} />)
+      ) : (
+        <p>No reviews match your search</p>
+      )}
+    </div>
+  );
 }
--- src/util/helper.js
+++ src/util/helper.js
@@ -1,5 +1,3 @@
-function filterReviews(reviews) {}
-//Ascending
 export function sortScoreByAscending(reviews) {
   let sortedReviews = reviews.sort((a, b) => {
     return a.score - b.score;
