90f8451147a90999027605391d3d8090c18268b3
tsbajwa <teranjit@gmail.com>
2017-11-14 19:39:58 +1100

    added styling setup

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import "./App.css";
 import MainContainer from "./components/containers/MainContainer";
 export default class App extends Component {
   render() {
-    return (
-      <div>
-        <MainContainer />
-      </div>
-    );
+    return <MainContainer />;
   }
 }
