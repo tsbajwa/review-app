6ecb481d644ba98fe0184de14d3ba7f5079067b2
tsbajwa <teranjit@gmail.com>
2017-11-15 07:25:13 +1100

    test

--- src/components/components/Filter.js
+++ src/components/components/Filter.js
@@ -1,15 +1,21 @@
 import React from "react";
 
 export default function Filter(props) {
+  const {
+    onAllReviewsClick,
+    onFavourableReviewsClick,
+    onUnfavourableClick,
+    selectedFilter,
+  } = props;
   return (
     <div className="filter-container">
-      <button className="filter-container__btn" onClick={props.onAllReviewsClick}>
+      <button className="filter-container__btn" onClick={onAllReviewsClick}>
         All Reviews
       </button>
-      <button className="filter-container__btn" onClick={props.onFavourableReviewsClick}>
+      <button className="filter-container__btn" onClick={onFavourableReviewsClick}>
         Positive Reviews
       </button>
-      <button className="filter-container__btn" onClick={props.onUnfavourableClick}>
+      <button className="filter-container__btn" onClick={onUnfavourableClick}>
         Negative Reviews
       </button>
     </div>
--- src/components/containers/FilterContainer.js
+++ src/components/containers/FilterContainer.js
@@ -3,21 +3,28 @@ import Filter from "../components/Filter";
 import { showFavourableReviews, showUnfavourableReviews } from "../../util/helper";
 
 export default class FilterContainer extends React.Component {
+  state = {
+    selectedFilter: "all",
+  };
   handleFavourableReviewsClick = () => {
     const filteredReviews = showFavourableReviews(this.props.reviews);
     this.props.handleFilteredReviewUpdate(filteredReviews);
+    this.setState({ selectedFilter: "favourable" });
   };
 
   handleUnfavourableReviewsClick = () => {
     const filteredReviews = showUnfavourableReviews(this.props.reviews);
     this.props.handleFilteredReviewUpdate(filteredReviews);
+    this.setState({ selectedFilter: "unfavourable" });
   };
   handleAllReviewsClick = () => {
     this.props.handleFilteredReviewUpdate(this.props.reviews);
+    this.setState({ selectedFilter: "all" });
   };
   render() {
     return (
       <Filter
+        selectedFilter={this.state.selectedFilter}
         onFavourableReviewsClick={this.handleFavourableReviewsClick}
         onUnfavourableClick={this.handleUnfavourableReviewsClick}
         onAllReviewsClick={this.handleAllReviewsClick}
